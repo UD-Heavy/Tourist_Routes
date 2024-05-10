50f6257082394e3093575e899347af1f17445ec7
Кирилл Тюменцев <584392@ad.kgsu.ru>
2024-05-10 20:54:54 +0500

    сделал вход и регистрацию и переход))

--- app/src/main/java/com/example/test/SecondActivity.java
+++ app/src/main/java/com/example/test/SecondActivity.java
@@ -1,10 +1,7 @@
 package com.example.test;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.view.WindowManager;
 import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
@@ -13,24 +10,21 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-
+public class SecondActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        Button button = findViewById(R.id.buttonRegister);
-        button.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.register_window);
+        Button button1 = findViewById(R.id.do_Register);
+        button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //setContentView(R.layout.register_window);
-                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-                startActivity(intent);
+                //setContentView(R.layout.activity_main);
+                finish();
             }
         });
-
     }
 
 }
