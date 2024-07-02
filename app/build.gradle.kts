e76da1e0a77104ab2b2444601d994a57bfb2e56f
zuqzwanq <zuqzwanq@icloud.com>
2024-07-02 22:22:48 +0500

    киримча сделана картошка новостей И ЕЩЕ ЧТОТО

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,12 +32,16 @@ android {
 }
 
 dependencies {
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+
 
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
     implementation(libs.firebase.database)
+    implementation(libs.coordinatorlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
---
+++ app/src/main/java/com/example/test/pages/NewsPage.java
@@ -0,0 +1,124 @@
+package com.example.test.pages;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.test.R;
+import com.example.test.models.MyData;
+
+import java.util.ArrayList;
+
+public class NewsPage extends AppCompatActivity {
+
+    RecyclerView rv;
+    ArrayList<MyData> news_list;
+    LinearLayoutManager linearLayoutManager;
+
+    MyRvAdapter news;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.news_page);
+
+
+        rv = findViewById(R.id.recycler_view);
+
+        // соединение с бд, получаем список объектов
+        // ArrayList<Document> DBList = dbRepositories.getAll();
+        // for (MyData data : DBList){
+        //      dataSourse.add(data.get("title"), convertBase64ToImage(data.get("image")))
+        // }
+        news_list = new ArrayList<>();
+        news_list.add(new MyData(getResources().getString(R.string.first_card), R.drawable.chas));
+        news_list.add(new MyData(getResources().getString(R.string.forgot_pass), R.drawable.aleksandra_nevskogo));
+        news_list.add(new MyData(getResources().getString(R.string.first_card), R.drawable.chas));
+        news_list.add(new MyData(getResources().getString(R.string.forgot_pass), R.drawable.aleksandra_nevskogo));
+        news_list.add(new MyData(getResources().getString(R.string.first_card), R.drawable.chas));
+        news_list.add(new MyData(getResources().getString(R.string.forgot_pass), R.drawable.aleksandra_nevskogo));
+        news_list.add(new MyData(getResources().getString(R.string.first_card), R.drawable.chas));
+        news_list.add(new MyData(getResources().getString(R.string.forgot_pass), R.drawable.aleksandra_nevskogo));
+        news_list.add(new MyData(getResources().getString(R.string.first_card), R.drawable.chas));
+        news_list.add(new MyData(getResources().getString(R.string.forgot_pass), R.drawable.aleksandra_nevskogo));
+        news_list.add(new MyData(getResources().getString(R.string.first_card), R.drawable.chas));
+        news_list.add(new MyData(getResources().getString(R.string.forgot_pass), R.drawable.aleksandra_nevskogo));
+
+
+
+        linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+
+        news = new MyRvAdapter(news_list);
+        linearLayoutManager.isAutoMeasureEnabled();
+        rv.setLayoutManager(linearLayoutManager);
+        rv.setNestedScrollingEnabled(false);
+        rv.setAdapter(news);
+
+    }
+    class MyRvAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+        private final ArrayList<MyData> data;
+
+        public MyRvAdapter(ArrayList<MyData> data) {
+            this.data = data;
+        }
+
+        @NonNull
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+
+
+            View view = inflater.inflate(R.layout.list_item_main_menu_card, parent, false);
+            return new MyRvAdapter.CardViewHolder(view);
+
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+            if (position < data.size()) {
+                MyData currentItem1 = data.get(position);
+                if (holder instanceof MyRvAdapter.CardViewHolder) {
+                    ((MyRvAdapter.CardViewHolder) holder).bind(currentItem1);
+                }
+            }
+        }
+
+        @Override
+        public int getItemCount() {
+            return data.size();
+        }
+
+
+
+        // ViewHolder для карточки без кнопки
+        class CardViewHolder extends RecyclerView.ViewHolder {
+            TextView tvTitle;
+            ImageView ivImage;
+
+            public CardViewHolder(@NonNull View itemView) {
+                super(itemView);
+                tvTitle = itemView.findViewById(R.id.text_view);
+                ivImage = itemView.findViewById(R.id.image_view);
+            }
+
+            public void bind(MyData item) {
+                tvTitle.setText(item.getTitle());
+                ivImage.setImageResource(item.getImageResId());
+            }
+        }
+
+
+    }
+}
